facbbbd875c1479ad837a0af1619e1e7b6312796
775479 <bhavani.2018.ece@anits.edu.in>
2023-03-07 14:26:48 +0530

    react6

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Headcheck from './components/CeckHead';
+import Footer1 from './components/Footer';
+import './App.css';
+import Demo from './components/DataEntry';
+function App() {
+  return (<div id="foot" ><div id='foot1'>
+  <Headcheck/><Demo/></div><Footer1/></div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import {Typography} from '@mui/material';
+import './Component.css';
+function Footer1(){
+    return(
+        <div style={{textAlign:'center',margin:80}}>
+                <Typography variant='p' >Copyright©<a href="#">Figure Website</a> 2022</Typography>
+        </div>   );
+}
+export default Footer1;
